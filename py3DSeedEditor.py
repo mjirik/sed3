f86a2d04656e68f17b6b84e0140df1eaec6297f4
Miroslav Jiřík <miroslav.jirik@gmail.com>
2012-12-06 23:19:21 +0100

    contour added

--- py3DSeedEditor.py
+++ py3DSeedEditor.py
@@ -4,7 +4,7 @@
 import unittest
 import sys
 sys.path.append("./src/")
-import pdb
+#import pdb
 #  pdb.set_trace();
 
 import scipy.io
@@ -44,7 +44,7 @@ class py3DSeedEditor:
     #    raise Exception('Input size error','Shape if input data and segmentation must be same')
 
     def __init__(self, img, voxelsizemm=[1,1,1], initslice = 0 , colorbar = True,
-            cmap = matplotlib.cm.Greys_r):
+            cmap = matplotlib.cm.Greys_r, seeds = None, contour = None):
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(111)
         if len(img.shape) == 2:
@@ -58,9 +58,17 @@ class py3DSeedEditor:
         self.actual_slice = initslice
         self.colorbar = colorbar
         self.cmap = cmap 
-        self.seeds = np.zeros(self.imgshape, np.int8)
+        if seeds == None:
+            self.seeds = np.zeros(self.imgshape, np.int8)
+        else:
+            self.seeds = seeds
         self.imgmax = np.max(img)
         self.imgmin = np.min(img)
+
+        """ Mapping mouse button to class number"""
+        self.button_map = {1:1, 2:3, 3:2, 4:4, 5:5, 6:6, 7:7, 8:8}
+
+        self.contour = contour
 
 
         self.press = None
@@ -69,7 +77,7 @@ class py3DSeedEditor:
         self.fig.subplots_adjust(left=0.25, bottom=0.25)
 
 
-        self.show_slice()
+        self.draw_slice()
 
         if self.colorbar:
             self.fig.colorbar(self.imsh)
@@ -89,16 +97,46 @@ class py3DSeedEditor:
         self.fig.canvas.mpl_connect('button_release_event', self.on_release)
         self.fig.canvas.mpl_connect('motion_notify_event', self.on_motion)
 
-        self.show_slice()
+        self.draw_slice()
 
 
-    def show_slice(self):
+    def update_slice(self):
+        #TODO tohle je tu kvuli contour, neumim ji odstranit jinak
+        self.ax.cla()
+
+        self.draw_slice()
+
+    def draw_slice(self):
         sliceimg = self.img[:,:,self.actual_slice]
-        self.imsh = self.ax.imshow(sliceimg, self.cmap, vmin = self.imgmin, vmax = self.imgmax)
+        #self.ax.cla()
+        self.imsh = self.ax.imshow(sliceimg, self.cmap, vmin = self.imgmin, vmax = self.imgmax, interpolation='nearest')
         #plt.hold(True)
         #pdb.set_trace();
-        self.ax.imshow(self.prepare_overlay(self.seeds[:,:,self.actual_slice]))
+        self.ax.imshow(self.prepare_overlay(self.seeds[:,:,self.actual_slice]), interpolation='nearest')
+
+        # vykreslení okraje
+        #X,Y = np.meshgrid(self.imgshape[0], self.imgshape[1])
+        
+        if self.contour != None:
+            try:
+                # exception catch problem with none object in image
+                ctr = self.ax.contour(self.contour[:,:,self.actual_slice],1, linewidths=2)
+            except:
+                pass
+
+        #print ctr
+        #import pdb; pdb.set_trace()
+
+
+
+
+
         self.fig.canvas.draw()
+        self.ax.cla()
+        #del(ctr)
+
+
+
         #pdb.set_trace();
         #plt.hold(False)
 
@@ -116,7 +154,7 @@ class py3DSeedEditor:
 # zaokrouhlení
         #self.actual_slice_slider.set_val(round(self.actual_slice_slider.val))
         self.actual_slice = round(val)
-        self.show_slice()
+        self.update_slice()
 
     def prepare_overlay(self,seeds):
         sh = list(seeds.shape)
@@ -189,11 +227,14 @@ class py3DSeedEditor:
 
         #plt.axes(self.ax)
         #plt.plot(x0, y0)
+        #button Mapping
+        btn = self.button_map[btn]
+
         self.set_seeds(y0, x0, self.actual_slice, btn )
         #self.fig.canvas.draw()
         #pdb.set_trace();
         self.press = None
-        self.show_slice()
+        self.update_slice()
 
     def set_seeds(self, px, py, pz, value = 1, voxelsizemm = [1,1,1], cursorsizemm = [1,1,1]):
         assert len(px) == len(py) , 'px and py describes a point, their size must be same'
@@ -202,9 +243,24 @@ class py3DSeedEditor:
             self.seeds[item, py[i], pz] = value
 
 
+# @todo 
+    def get_seed_sub(self, label):
+        """ Return list of all seeds with specific label
+        """
+        sx, sy, sz = np.nonzero(self.seeds == label)
+
+        return sx, sy, sz
+
+    def get_seed_val(self, label):
+        """ Return data values for specific seed label"""
+        return self.img[self.seeds==label]
+            
+
 #self.rect.figure.canvas.draw()
 
     #return data 
+
+
 
 # --------------------------tests-----------------------------
 class Tests(unittest.TestCase):
@@ -238,6 +294,55 @@ class Tests(unittest.TestCase):
         onePixel = overlay[30,40]
         self.assertTrue(all(onePixel == [1,0,0,1]))
 
+    def test_get_seed_sub(self):
+        """ Testuje, jestli funkce pro vracení dat funguje správně, 
+        je to zkoušeno na konkrétních hodnotách
+        """
+        val = 7
+        self.ed.set_seeds([10,12,13],[13,13,15], 3, value=val)
+        seedsx, seedsy, seedsz = self.ed.get_seed_sub(val)
+
+        found = [False, False, False]
+        for i in range(len(seedsx)):
+            if (seedsx[i] == 10) & (seedsy[i] == 13) & (seedsz[i] == 3):
+                found[0] = True
+            if (seedsx[i] == 12) & (seedsy[i] == 13) & (seedsz[i] == 3):
+                found[1] = True
+            if (seedsx[i] == 13) & (seedsy[i] == 15) & (seedsz[i] == 3):
+                found[2] = True
+
+        logger.debug(found)
+
+        self.assertTrue(all(found))
+
+        
+    def test_get_seed_val(self):
+        """ Testuje, jestli jsou správně vraceny hodnoty pro označené pixely
+        je to zkoušeno na konkrétních hodnotách
+        """
+        label = 7
+        self.ed.set_seeds([11],[14], 4, value=label)
+        seedsx, seedsy, seedsz = self.ed.get_seed_sub(label)
+
+        val = self.ed.get_seed_val(label)
+        expected_val = self.ed.img[11,14,4]
+
+        logger.debug(val)
+        logger.debug(expected_val)
+
+        self.assertIn(expected_val, val)
+       # found = False
+       # for i in range(len(val)):
+       #     if (val[i] == :
+       #         found = True
+
+
+       # self.assertTrue(found)
+        
+
+
+        
+
 
 
 #
@@ -249,6 +354,15 @@ class Tests(unittest.TestCase):
 #
         
         
+def generate_data(shp=[16,16,16]):
+    """ Generating data """
+
+    x = np.ones(shp)
+# inserting box
+    x[4:-4, 6:-2, 1:-6] = -1
+    x_noisy = x + np.random.normal(0, 0.6, size=x.shape)
+    return x_noisy
+
 # --------------------------main------------------------------
 if __name__ == "__main__":
     logger = logging.getLogger()
@@ -276,6 +390,8 @@ if __name__ == "__main__":
             help='*.mat file with variables "data", "segmentation" and "threshod"')
     parser.add_argument('-d', '--debug', action='store_true',
             help='run in debug mode')
+    parser.add_argument('-e3', '--example3d', action='store_true',
+            help='run with 3D example data')
     parser.add_argument('-t', '--tests', action='store_true', 
             help='run unittest')
     parser.add_argument('-o', '--outputfile', type=str,
@@ -291,7 +407,9 @@ if __name__ == "__main__":
         sys.argv[1:]=[]
         unittest.main()
 
-    if args.filename == 'lena':
+    if args.example3d:
+        data = generate_data()
+    elif args.filename == 'lena':
         from scipy import misc
         data = misc.lena()
     else:
@@ -318,4 +436,5 @@ if __name__ == "__main__":
     output = pyed.show()
 
     scipy.io.savemat(args.outputfile,{'data':output})
+    pyed.get_seed_val(1)
 
