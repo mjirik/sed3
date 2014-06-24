1644657909e395d13d07e704914fc3e4c7250e0d
M. Jirik <miroslav.jirik@gmail.com>
2014-06-24 23:12:05 +0200

    pep8

--- py3DSeedEditor.py
+++ py3DSeedEditor.py
@@ -4,8 +4,8 @@
 import unittest
 import sys
 sys.path.append("./src/")
-#import pdb
-#  pdb.set_trace();
+# import pdb
+# pdb.set_trace();
 
 import scipy.io
 
@@ -40,13 +40,13 @@ class py3DSeedEditor:
     selected_seeds = ed.seeds
 
     """
-    #if data.shape != segmentation.shape:
+    # if data.shape != segmentation.shape:
     #    raise Exception('Input size error','Shape if input data and segmentation must be same')
 
 
-    def __init__(self, img, voxelsizemm=[1,1,1], initslice = 0 , colorbar = True,
+    def __init__(self, img, voxelsizemm=[1, 1, 1], initslice = 0 , colorbar = True,
             cmap = matplotlib.cm.Greys_r, seeds = None, contour = None, zaxis=0,
-            mouse_button_map= {1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8},
+            mouse_button_map= {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8},
             windowW = [], windowC = []
             ):
         self.fig = plt.figure()
@@ -54,11 +54,11 @@ class py3DSeedEditor:
         if len(img.shape) == 2:
             imgtmp = img
             img = np.zeros([1, imgtmp.shape[0], imgtmp.shape[1]])
-            #self.imgshape.append(1)
+            # self.imgshape.append(1)
             img[-1,:,:] = imgtmp
 
             zaxis = 0
-            #pdb.set_trace();
+            # pdb.set_trace();
 
         # Rotate data in depndecy on zaxispyplot
         img = self._rotate_start(img, zaxis)
@@ -68,7 +68,7 @@ class py3DSeedEditor:
         self.rotated_back = False
         self.zaxis = zaxis
 
-        #self.ax = self.fig.add_subplot(111)
+        # self.ax = self.fig.add_subplot(111)
         self.imgshape = list(img.shape)
         self.img = img
         self.actual_slice = initslice
@@ -97,10 +97,10 @@ class py3DSeedEditor:
 
 
 # language
-        self.texts = {'btn_delete':'Delete', 'btn_close': 'Close'}
+        self.texts = {'btn_delete': 'Delete', 'btn_close': 'Close'}
 
-        #iself.fig.subplots_adjust(left=0.25, bottom=0.25)
-        self.ax = self.fig.add_axes([0.2, 0.3, 0.7,0.6])
+        # iself.fig.subplots_adjust(left=0.25, bottom=0.25)
+        self.ax = self.fig.add_axes([0.2, 0.3, 0.7, 0.6])
 
 
         self.draw_slice()
@@ -125,12 +125,12 @@ class py3DSeedEditor:
 
 
 # delete seeds
-        self.ax_delete_seeds = self.fig.add_axes([0.2,0.1,0.1,0.075])
+        self.ax_delete_seeds = self.fig.add_axes([0.2, 0.1, 0.1, 0.075])
         self.btn_delete = Button(self.ax_delete_seeds, self.texts['btn_delete'])
         self.btn_delete.on_clicked(self.callback_delete)
 
 # close button
-        self.ax_delete_seeds = self.fig.add_axes([0.7,0.1,0.1,0.075])
+        self.ax_delete_seeds = self.fig.add_axes([0.7, 0.1, 0.1, 0.075])
         self.btn_delete = Button(self.ax_delete_seeds, self.texts['btn_close'])
         self.btn_delete.on_clicked(self.callback_close)
 
@@ -139,7 +139,7 @@ class py3DSeedEditor:
     def _rotate_start(self, data, zaxis):
         if data != None:
             if zaxis == 0:
-                data = np.transpose(data,(1,2,0))
+                data = np.transpose(data, (1, 2, 0))
             elif zaxis == 2:
                 pass
             else:
@@ -151,7 +151,7 @@ class py3DSeedEditor:
         if data != None:
             if self.rotated_back == False:
                 if zaxis == 0:
-                    data = np.transpose(data,(2,0,1))
+                    data = np.transpose(data, (2, 0, 1))
                 elif zaxis == 2:
                     pass
                 else:
@@ -163,45 +163,45 @@ class py3DSeedEditor:
         return data
 
     def update_slice(self):
-        #TODO tohle je tu kvuli contour, neumim ji odstranit jinak
+        # TODO tohle je tu kvuli contour, neumim ji odstranit jinak
         self.ax.cla()
 
         self.draw_slice()
 
     def draw_slice(self):
-        sliceimg = self.img[:, :, int(self.actual_slice)]
+        sliceimg = self.img[:,:, int(self.actual_slice)]
         self.imsh = self.ax.imshow(sliceimg, self.cmap, vmin = self.imgmin, vmax = self.imgmax, interpolation='nearest')
-        #plt.hold(True)
-        #pdb.set_trace();
+        # plt.hold(True)
+        # pdb.set_trace();
         self.ax.imshow(self.prepare_overlay(
-            self.seeds[:, :, int(self.actual_slice)]
+            self.seeds[:,:, int(self.actual_slice)]
         ), interpolation='nearest', vmin=self.imgmin, vmax=self.imgmax)
 
         # vykreslení okraje
-        #X,Y = np.meshgrid(self.imgshape[0], self.imgshape[1])
+        # X,Y = np.meshgrid(self.imgshape[0], self.imgshape[1])
 
         if self.contour != None:
             try:
                 # exception catch problem with none object in image
-                ctr = self.ax.contour(self.contour[:,:,self.actual_slice],1, linewidths=2)
+                ctr = self.ax.contour(self.contour[:,:, int(self.actual_slice)], 1, linewidths=2)
             except:
                 pass
 
-        #print ctr
-        #import pdb; pdb.set_trace()
+        # print ctr
+        # import pdb; pdb.set_trace()
 
 
 
 
 
         self.fig.canvas.draw()
-        #self.ax.cla()
-        #del(ctr)
+        # self.ax.cla()
+        # del(ctr)
 
 
 
-        #pdb.set_trace();
-        #plt.hold(False)
+        # pdb.set_trace();
+        # plt.hold(False)
 
     def next_slice(self):
         self.actual_slice = self.actual_slice + 1
@@ -215,11 +215,11 @@ class py3DSeedEditor:
 
     def sliceslider_update(self, val):
 # zaokrouhlení
-        #self.actual_slice_slider.set_val(round(self.actual_slice_slider.val))
+        # self.actual_slice_slider.set_val(round(self.actual_slice_slider.val))
         self.actual_slice = round(val)
         self.update_slice()
 
-    def prepare_overlay(self,seeds):
+    def prepare_overlay(self, seeds):
         sh = list(seeds.shape)
         if len(sh) == 2:
             sh.append(4)
@@ -229,11 +229,11 @@ class py3DSeedEditor:
         # sh[2] = 4
         overlay = np.zeros(sh)
 
-        overlay[:,:,0] = (seeds == 1)
-        overlay[:,:,1] = (seeds == 2)
-        overlay[:,:,2] = (seeds == 3)
+        overlay[:,:, 0] = (seeds == 1)
+        overlay[:,:, 1] = (seeds == 2)
+        overlay[:,:, 2] = (seeds == 3)
 
-        overlay[:,:,3] = (seeds > 0)
+        overlay[:,:, 3] = (seeds > 0)
 
         return overlay
 
@@ -257,27 +257,30 @@ class py3DSeedEditor:
         if event.button == 'down':
             self.prev_slice()
         self.actual_slice_slider.set_val (self.actual_slice)
-        #tim, ze dojde ke zmene slideru je show_slce volan z nej
-        #self.show_slice()
-        #print self.actual_slice
+        # tim, ze dojde ke zmene slideru je show_slce volan z nej
+        # self.show_slice()
+        # print self.actual_slice
 
 
-## malování -------------------
+# malování -------------------
     def on_press(self, event):
         'on but-ton press we will see if the mouse is over us and store some data'
-        if event.inaxes != self.ax: return
-        #contains, attrd = self.rect.contains(event)
-        #if not contains: return
-        #print 'event contains', self.rect.xy
-        #x0, y0 = self.rect.xy
+        if event.inaxes != self.ax:
+            return
+        # contains, attrd = self.rect.contains(event)
+        # if not contains: return
+        # print 'event contains', self.rect.xy
+        # x0, y0 = self.rect.xy
         self.press = [event.xdata], [event.ydata], event.button
-        #self.press1 = True
+        # self.press1 = True
     def on_motion(self, event):
         'on motion we will move the rect if the mouse is over us'
-        if self.press is None: return
+        if self.press is None:
+            return
 
-        if event.inaxes != self.ax: return
-        #print event.inaxes
+        if event.inaxes != self.ax:
+            return
+        # print event.inaxes
 
         x0, y0, btn = self.press
         x0.append(event.xdata)
@@ -285,27 +288,28 @@ class py3DSeedEditor:
 
     def on_release(self, event):
         'on release we reset the press data'
-        if self.press is None: return
-        #print self.press
+        if self.press is None:
+            return
+        # print self.press
         x0, y0, btn = self.press
         if btn == 1:
             color = 'r'
         elif btn == 2:
             color = 'b'
 
-        #plt.axes(self.ax)
-        #plt.plot(x0, y0)
-        #button Mapping
+        # plt.axes(self.ax)
+        # plt.plot(x0, y0)
+        # button Mapping
         btn = self.button_map[btn]
 
         self.set_seeds(y0, x0, self.actual_slice, btn )
-        #self.fig.canvas.draw()
-        #pdb.set_trace();
+        # self.fig.canvas.draw()
+        # pdb.set_trace();
         self.press = None
         self.update_slice()
 
     def callback_delete(self, event):
-        self.seeds[:,:,self.actual_slice] = 0
+        self.seeds[:,:, self.actual_slice] = 0
         self.update_slice()
 
 
@@ -313,8 +317,8 @@ class py3DSeedEditor:
         matplotlib.pyplot.clf()
         matplotlib.pyplot.close()
 
-    def set_seeds(self, px, py, pz, value = 1, voxelsizemm = [1,1,1], cursorsizemm = [1,1,1]):
-        assert len(px) == len(py) , 'px and py describes a point, their size must be same'
+    def set_seeds(self, px, py, pz, value = 1, voxelsizemm = [1, 1, 1], cursorsizemm = [1, 1, 1]):
+        assert len(px) == len(py), 'px and py describes a point, their size must be same'
 
         for i, item in enumerate(px):
             self.seeds[int(item), int(py[i]), int(pz)] = value
@@ -330,12 +334,12 @@ class py3DSeedEditor:
 
     def get_seed_val(self, label):
         """ Return data values for specific seed label"""
-        return self.img[self.seeds==label]
+        return self.img[self.seeds == label]
 
 
-#self.rect.figure.canvas.draw()
+# self.rect.figure.canvas.draw()
 
-    #return data
+    # return data
 
 
 
@@ -345,38 +349,38 @@ class Tests(unittest.TestCase):
         pass
     def setUp(self):
         """ Nastavení společných proměnných pro testy  """
-        datashape = [120,85,30]
+        datashape = [120, 85, 30]
         self.datashape = datashape
         self.rnddata = np.random.rand(datashape[0], datashape[1], datashape[2])
         self.segmcube = np.zeros(datashape)
-        self.segmcube[30:70, 40:60,5:15] = 1
+        self.segmcube[30:70, 40:60, 5:15] = 1
 
         self.ed = py3DSeedEditor(self.rnddata)
-        #ed.show()
-        #selected_seeds = ed.seeds
+        # ed.show()
+        # selected_seeds = ed.seeds
 
     def test_same_size_input_and_output(self):
         """Funkce testuje stejnost vstupních a výstupních dat"""
-        #outputdata = vesselSegmentation(self.rnddata,self.segmcube)
+        # outputdata = vesselSegmentation(self.rnddata,self.segmcube)
         self.assertEqual(self.ed.seeds.shape, self.rnddata.shape)
     def test_set_seeds(self):
         ''' Testuje uložení do seedů '''
         val = 7
-        self.ed.set_seeds([10,12,13],[13,13,15], 3, value=val)
-        self.assertEqual(self.ed.seeds[10,13,3],val)
+        self.ed.set_seeds([10, 12, 13], [13, 13, 15], 3, value=val)
+        self.assertEqual(self.ed.seeds[10, 13, 3], val)
 
     def test_prepare_overlay(self):
         ''' Testuje vytvoření rgba obrázku z labelů'''
-        overlay = self.ed.prepare_overlay(self.segmcube[:,:,6])
-        onePixel = overlay[30,40]
-        self.assertTrue(all(onePixel == [1,0,0,1]))
+        overlay = self.ed.prepare_overlay(self.segmcube[:,:, 6])
+        onePixel = overlay[30, 40]
+        self.assertTrue(all(onePixel == [1, 0, 0, 1]))
 
     def test_get_seed_sub(self):
         """ Testuje, jestli funkce pro vracení dat funguje správně,
         je to zkoušeno na konkrétních hodnotách
         """
         val = 7
-        self.ed.set_seeds([10,12,13],[13,13,15], 3, value=val)
+        self.ed.set_seeds([10, 12, 13], [13, 13, 15], 3, value=val)
         seedsx, seedsy, seedsz = self.ed.get_seed_sub(val)
 
         found = [False, False, False]
@@ -398,11 +402,11 @@ class Tests(unittest.TestCase):
         je to zkoušeno na konkrétních hodnotách
         """
         label = 7
-        self.ed.set_seeds([11],[14], 4, value=label)
+        self.ed.set_seeds([11], [14], 4, value=label)
         seedsx, seedsy, seedsz = self.ed.get_seed_sub(label)
 
         val = self.ed.get_seed_val(label)
-        expected_val = self.ed.img[11,14,4]
+        expected_val = self.ed.img[11, 14, 4]
 
         logger.debug(val)
         logger.debug(expected_val)
@@ -431,7 +435,7 @@ class Tests(unittest.TestCase):
 #
 
 
-def generate_data(shp=[16,20,24]):
+def generate_data(shp=[16, 20, 24]):
     """ Generating data """
 
     x = np.ones(shp)
@@ -445,11 +449,11 @@ if __name__ == "__main__":
     logger = logging.getLogger()
     logger.setLevel(logging.WARNING)
 # při vývoji si necháme vypisovat všechny hlášky
-    #logger.setLevel(logging.DEBUG)
+    # logger.setLevel(logging.DEBUG)
 
     ch = logging.StreamHandler()
 #   output configureation
-    #logging.basicConfig(format='%(asctime)s %(message)s')
+    # logging.basicConfig(format='%(asctime)s %(message)s')
     logging.basicConfig(format='%(message)s')
 
     formatter = logging.Formatter("%(levelname)-5s [%(module)s:%(funcName)s:%(lineno)d] %(message)s")
@@ -461,8 +465,8 @@ if __name__ == "__main__":
 
     # input parser
     parser = argparse.ArgumentParser(description='Segment vessels from liver. For example call py3DSeedEditor -f lena')
-    parser.add_argument('-f','--filename',
-            #default = '../jatra/main/step.mat',
+    parser.add_argument('-f', '--filename',
+            # default = '../jatra/main/step.mat',
             default = 'lena',
             help='*.mat file with variables "data", "segmentation" and "threshod"')
     parser.add_argument('-d', '--debug', action='store_true',
@@ -472,7 +476,7 @@ if __name__ == "__main__":
     parser.add_argument('-t', '--tests', action='store_true',
             help='run unittest')
     parser.add_argument('-o', '--outputfile', type=str,
-        default='output.mat',help='output file name')
+        default='output.mat', help='output file name')
     args = parser.parse_args()
 
 
@@ -481,7 +485,7 @@ if __name__ == "__main__":
 
     if args.tests:
         # hack for use argparse and unittest in one module
-        sys.argv[1:]=[]
+        sys.argv[1:] = []
         unittest.main()
 
     if args.example3d:
@@ -498,20 +502,20 @@ if __name__ == "__main__":
         dataraw = scipy.io.loadmat(args.filename, variable_names=['data'])
         data = dataraw['data']
 
-        #logger.debug(matthreshold['threshold'][0][0])
+        # logger.debug(matthreshold['threshold'][0][0])
 
 
         # zastavení chodu programu pro potřeby debugu,
         # ovládá se klávesou's','c',...
         # zakomentovat
-        #pdb.set_trace();
+        # pdb.set_trace();
 
         # zde by byl prostor pro ruční (interaktivní) zvolení prahu z klávesnice
-        #tě ebo jinak
+        # tě ebo jinak
 
     pyed = py3DSeedEditor(data)
     output = pyed.show()
 
-    scipy.io.savemat(args.outputfile,{'data':output})
+    scipy.io.savemat(args.outputfile, {'data':output})
     pyed.get_seed_val(1)
 
