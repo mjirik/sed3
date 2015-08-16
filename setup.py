7110756e00748dd25a3ff9e351f6c63dbb067f8c
M. Jirik <miroslav.jirik@gmail.com>
2015-08-16 15:13:05 +0200

    more dependencies filled for anaconda skeleton process

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.1.8',
+    version='1.1.9',
     url='https://github.com/mjirik/py3DSeedEditor',
     author='Miroslav Jirik',
     author_email='miroslav.jirik@gmail.com',
@@ -55,7 +55,8 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=['matplotlib', 'pyyaml'],
+    install_requires=['matplotlib', 'pyyaml', 'scipy'],
+    test_require=['nose', 'mock']
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
