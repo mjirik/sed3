2020cdd1a56d0f24fbb5b1a5deeb2af7987981e8
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-03-19 15:32:58 +0100

    pyqt4 added to setup

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=['matplotlib', 'pyyaml', 'scipy', "pyqt>4,<5"
+    install_requires=['matplotlib', 'pyyaml', 'scipy', "PyQt4"
                       # - python
                       # - matplotlib
                       # - pyyaml
